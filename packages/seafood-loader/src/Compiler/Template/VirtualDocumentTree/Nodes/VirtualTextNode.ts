02f6bd9975ca48680ac44bf609378213b3f1b402
Aliaksandr Parfiankou <aliaksandr.parfiankou@gmail.com>
2018-12-24 15:28:19 +0300

    add text node data binding

--- packages/seafood-loader/src/Compiler/Template/VirtualDocumentTree/Nodes/VirtualTextNode.ts
+++ packages/seafood-loader/src/Compiler/Template/VirtualDocumentTree/Nodes/VirtualTextNode.ts
@@ -1,7 +1,68 @@
 import {SingleVirtualNode} from "../SingleVirtualNode";
 
 export class VirtualTextNode extends SingleVirtualNode {
+    protected expressionResults: any[];
+    protected expressions: any[];
+
+    protected injectionFinderRegExp = new RegExp(/{{([\S\s]*?)}}/, "gm");
+
+    public constructor() {
+        super();
+        this.expressions = [];
+        this.expressionResults = [];
+    }
+
+    public beforeRender(): void {
+        super.beforeRender();
+        this.bindReactiveExpressions();
+    }
+
     protected buildNode(): Node {
-        return document.createTextNode(this.parsedNode.data);
+        let expressionIndex = 0;
+        const text = this.parsedNode.data.replace(this.injectionFinderRegExp, () => {
+            let value = this.expressionResults[expressionIndex];
+
+
+            if (typeof value === "object") {
+                value = JSON.stringify(value);
+            }
+
+            expressionIndex++;
+
+            return value;
+        });
+        return document.createTextNode(text);
+    }
+
+    protected bindReactiveExpressions(): void {
+        this.expressions = this.getReactiveExpressionValues();
+        const scope = this.getScope();
+
+        for (const expressionIndex in this.expressions) {
+            if (this.expressions.hasOwnProperty(expressionIndex)) {
+                const expression = this.expressions[expressionIndex];
+                this.expressionResults[expressionIndex] =
+                    scope.executeExpression(expression, (value: any) => {
+                        this.expressionResults[expressionIndex] = value;
+                        this.removeBuildedNodeFromDom();
+                        this.render();
+                    });
+            }
+        }
+    }
+
+    protected getReactiveExpressionValues(): any[] {
+        let expressions: any[] = [];
+
+        if (this.parsedNode.data) {
+            const matches = this.parsedNode.data.match(this.injectionFinderRegExp);
+            if (matches) {
+                expressions = matches.map((expression: string) => {
+                    return expression.slice(2, -2).trim();
+                });
+            }
+        }
+
+        return expressions;
     }
 }
