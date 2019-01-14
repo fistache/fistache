fdc0b1dcd235610db70e088d653275ca80a95c56
Aliaksandr Parfiankou <aliaksandr.parfiankou@gmail.com>
2019-01-14 14:29:45 +0300

    wip

--- packages/seafood-loader/src/Compiler/Template/Renderer/VirtualElement/VirtualPackage.ts
+++ packages/seafood-loader/src/Compiler/Template/Renderer/VirtualElement/VirtualPackage.ts
@@ -75,11 +75,40 @@ export class VirtualPackage extends VirtualElement {
     }
 
     private appendForInAttribute() {
-        //
+        const expressionParts = this.forExpressionOrigin.split(' in ', 2)
+        const variable = expressionParts[0]
+        const expression = expressionParts[1]
+
+        if (variable.length && expression.length) {
+            const expressionResult = this.getScope().executeExpression(expression, (value: any, depth?: number) => {
+                if (!depth || depth <= 1) {
+                    this.updateForInExpression(value)
+                }
+            })
+
+            this.forExpressionResult = {
+                variable,
+                value: expressionResult
+            }
+
+            this.renderForInExpression()
+        } else {
+            console.warn('Variable name or expression is not provided in @for..in attribute.')
+        }
     }
 
     private appendForNAttribute() {
-        //
+        const expressionResult = this.getScope().executeExpression(
+            this.forExpressionOrigin,
+            (value: any) => {
+                this.updateForNExpression(value)
+        })
+
+        this.forExpressionResult = {
+            value: expressionResult
+        }
+
+        this.renderForNExpression()
     }
 
     private renderForOfExpression(isItUpdate: boolean = false) {
@@ -98,11 +127,95 @@ export class VirtualPackage extends VirtualElement {
         }
     }
 
+    private renderForInExpression(isItUpdate: boolean = false) {
+        const expressionResult = this.forExpressionResult as ForExpressionResult
+
+        if (Number.isInteger(expressionResult.value) && expressionResult.value >= 0) {
+            this.renderForNExpression(isItUpdate)
+        } else {
+            for (const index in expressionResult.value) {
+                if (expressionResult.value.hasOwnProperty(index) && !this.childVirtualNodes.hasOwnProperty(index)) {
+                    const virtualNode = this.renderMaquette(+index, {
+                        variable: expressionResult.variable,
+                        value: index
+                    })
+                    if (isItUpdate) {
+                        this.renderFragmentOnTree(virtualNode)
+                    }
+                }
+            }
+        }
+    }
+
+    private renderForNExpression(isItUpdate: boolean = false) {
+        if (this.forExpressionResult &&
+            Number.isInteger(this.forExpressionResult.value)
+        ) {
+            for (let i = this.childVirtualNodes.length; i < this.forExpressionResult.value; i++) {
+                let virtualNode
+                if (this.forExpressionResult.variable) {
+                    virtualNode = this.renderMaquette(+i, {
+                        variable: this.forExpressionResult.variable,
+                        value: i
+                    })
+                } else {
+                    virtualNode = this.renderMaquette(+i)
+                }
+                if (isItUpdate) {
+                    this.renderFragmentOnTree(virtualNode)
+                }
+            }
+        } else {
+            this.cleanCollectionWholly()
+        }
+    }
+
     private updateForOfExpression(value: any) {
         (this.forExpressionResult as ForExpressionResult).value = value
         this.updateForExpression(value, () => {
             this.renderForOfExpression(true)
         })
+    }
+
+    private updateForInExpression(value: any) {
+        (this.forExpressionResult as ForExpressionResult).value = value
+
+        if (Number.isInteger(value)) {
+            this.updateForNExpression(value, false)
+            this.renderForInExpression(true)
+        } else {
+            this.updateForExpression(value, () => {
+                this.renderForInExpression(true)
+            })
+        }
+    }
+
+    private updateForNExpression(value: any, render: boolean = true) {
+        if (Number.isInteger(value)) {
+            if (value < 0) {
+                value = 0
+            }
+
+            (this.forExpressionResult as ForExpressionResult).value = value
+            const previousValue = this.childVirtualNodes.length
+            const difference = value - previousValue
+
+            if (difference < 0) {
+                const rudenantIndecies: any[] = []
+
+                for (let i = value; i < previousValue; i++) {
+                    rudenantIndecies.push(i)
+                }
+
+                this.cleanCollection(rudenantIndecies)
+            }
+
+            if (render) {
+                this.renderForNExpression(true)
+            }
+        } else {
+            console.warn('Value passed in @for is not a number.')
+        }
     }
 
     private updateForExpression(
@@ -142,7 +255,7 @@ export class VirtualPackage extends VirtualElement {
         const collection = this.childVirtualNodes.slice()
 
         for (const index of rudenantIndecies) {
-            collection[index].delete()
+            this.childVirtualNodes[index].delete()
             collection.splice(index, 1)
 
             if (callback) {
@@ -153,14 +266,23 @@ export class VirtualPackage extends VirtualElement {
         this.childVirtualNodes = collection
     }
 
-    private renderMaquette(secondaryPosition: number, expressionResult: ForExpressionResult): VirtualNode {
+    private cleanCollectionWholly() {
+        for (const index in this.childVirtualNodes) {
+            if (this.childVirtualNodes.hasOwnProperty(index)) {
+                this.childVirtualNodes[index].delete()
+            }
+        }
+        this.childVirtualNodes = []
+    }
+
+    private renderMaquette(secondaryPosition: number, expressionResult?: ForExpressionResult): VirtualNode {
         const clonedVirtualNode = this.maquetteVirtualElement.clone()
 
         if (clonedVirtualNode instanceof VirtualElement) {
             clonedVirtualNode.getAttibuteContainer().extend(this.getAttibuteContainer())
         }
 
-        if (expressionResult.variable) {
+        if (expressionResult && expressionResult.variable) {
             clonedVirtualNode.getScope().setVariable(expressionResult.variable, expressionResult.value)
         }
 
