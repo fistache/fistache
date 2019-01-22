9b3f05e171f0176ad7e687203e75567f3e08ec2d
Aliaksandr Parfiankou <aliaksandr.parfiankou@gmail.com>
2019-01-22 15:59:41 +0300

    wip

--- packages/seafood-loader/src/Compiler/Template/Renderer/VirtualElement/VirtualEmbeddedContentNode.ts
+++ packages/seafood-loader/src/Compiler/Template/Renderer/VirtualElement/VirtualEmbeddedContentNode.ts
@@ -3,11 +3,6 @@ import { VirtualNode } from './VirtualNode'
 // todo: redesign this class
 export class VirtualEmbeddedContentNode extends VirtualNode {
     public makeNode(): Node {
-        return document.createComment('embedded content')
-    }
-
-    public getChildVirtualNodes(): VirtualNode[] {
-        console.log(super.getChildVirtualNodes())
-        return super.getChildVirtualNodes()
+        return document.createTextNode('')
     }
 }
